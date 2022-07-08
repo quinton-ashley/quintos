0fc9e94522534cd4a7dcbe3981937d4637f10c55
Quinton Ashley <qashto@gmail.com>
2022-07-08 09:09:21 -0500

    3.0.10

--- QuintOS.js
+++ QuintOS.js
@@ -31,9 +31,9 @@ QuintOS.levels = [
 {
 	let url = location.href.split('?');
 	QuintOS.web ??=
-		location.hostname != 'localhost' &&
-		location.hostname != '127.0.0.1' &&
-		(!url[1] || location.href.slice(-9) != 'home.html');
+		// location.hostname != 'localhost' &&
+		// location.hostname != '127.0.0.1' &&
+		!url[1] || location.href.slice(-9) != 'home.html';
 
 	if (!QuintOS.web) {
 		$('head').append('<link rel="icon" href="node_modules/quintos/favicon.png" />');
